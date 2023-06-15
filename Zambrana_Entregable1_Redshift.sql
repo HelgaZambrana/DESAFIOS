670ac1c48e59c2cddcc1e9e066c0abaf1424f597
Helga Zambrana <hvzambrana@gmail.com>
2023-06-15 00:26:04 -0300

    SQL

---
+++ Zambrana_Entregable1_Redshift.sql
@@ -0,0 +1,8 @@
+create table hvzambrana_coderhouse.alphavantage (
+	fecha timestamp null,
+	apertura float(15) null,
+	maximo float (15) null,
+	minimo float (15) null,
+	cierre float (15) null,
+	volumen float (15) null
+	)
