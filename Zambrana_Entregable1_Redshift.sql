a6359b7960c95c9552ae17f4998feff42c755d1f
Helga Zambrana <hvzambrana@gmail.com>
2023-10-10 23:28:18 -0300

    cambios

--- Zambrana_Entregable1_Redshift.sql
+++ Zambrana_Entregable1_Redshift.sql
@@ -1,8 +1,12 @@
-create table hvzambrana_coderhouse.alphavantage (
-	fecha timestamp null,
-	apertura float(15) null,
-	maximo float (15) null,
-	minimo float (15) null,
-	cierre float (15) null,
-	volumen float (15) null
-	)
+CREATE TABLE IF NOT EXISTS hvzambrana_coderhouse.alphavantage (
+    simbolo VARCHAR(10) NULL,
+    fecha TIMESTAMP NULL,
+    apertura FLOAT(15) NULL,
+    maximo FLOAT(15) NULL,
+    minimo FLOAT(15) NULL,
+    cierre FLOAT(15) NULL,
+    volumen FLOAT(15) NULL
+)
+
+DISTKEY (simbolo) -- Definir DISTKEY
+SORTKEY (simbolo, fecha); -- Definir SORTKEY
